d774713ea7665a60aa682ab6e2cd3d59dee34a37
3mz-ui <15217575952@163.com>
2025-12-23 23:50:23 +0800

    3mz

--- backend/config.h
+++ backend/config.h
@@ -3,9 +3,10 @@
 
 #include <string>
 
-// 数据文件路径（请根据实际环境修改）
-const std::string BOOKS_PATH = "C:\\Users\\3mz\\source\\repos\\finwork\\finwork\\item.csv";
-const std::string READERS_PATH = "C:\\Users\\3mz\\source\\repos\\finwork\\finwork\\uesr.csv";
-const std::string RECORDS_PATH = "C:\\Users\\3mz\\Desktop\\Project1\\cleaned_data.csv";
+// 数据文件路径（根据实际路径修改）
+const std::string BOOKS_PATH = "item1.csv";
+const std::string READERS_PATH = "user.csv";
+const std::string RECORDS_PATH = "records.csv";
+const std::string DATA_FOLDER = "data";
 
-#endif // CONFIG_H#pragma once
+#endif // CONFIG_H
